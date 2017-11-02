97df97a1182ccad6afbacea6bcb8db09563167b8
RichardM8114 <ramihelic@verizon.net>
2017-11-02 17:05:38 -0500

    Transferred OpModes from Oct 28th scrimmage.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionOpmode.java
@@ -0,0 +1,172 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Use This!", group="Iterative Opmode")
+//@Disabled
+public class CompetitionOpmode extends OpMode
+{
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor pitch = null;
+    private DcMotor extension = null;
+    private Servo leftClaw = null;
+    private Servo rightClaw = null;//NightClub
+    ColorSensor color;
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        pitch  = hardwareMap.get(DcMotor.class, "pitch");
+        extension = hardwareMap.get(DcMotor.class, "extension");
+        leftClaw  = hardwareMap.get(Servo.class, "leftClaw");
+        rightClaw = hardwareMap.get(Servo.class, "rightClaw");
+        color = hardwareMap.get(ColorSensor.class, "color");
+
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftClaw.setDirection(Servo.Direction.REVERSE);
+
+
+//        pitch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        pitch.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        pitch.setTargetPosition(0);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+/*        int min = 0;
+        int max = 300;
+
+        if(gamepad1.a){
+            pitch.setTargetPosition(max);
+            pitch.setPower(0.7);
+
+        }
+        else if(gamepad1.y){
+            pitch.setTargetPosition(min);
+            pitch.setPower(0.7);
+        }
+        else if(gamepad1.x){
+            pitch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            pitch.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            pitch.setTargetPosition(0);
+            pitch.setPower(0);
+        }
+        else{
+            pitch.setPower(0.7);
+            if(pitch.getCurrentPosition()>=pitch.getTargetPosition()-10 && pitch.getCurrentPosition()<=pitch.getTargetPosition()+10){
+                pitch.setPower(0);
+            }//This probably causes more issues than it fixes
+        }*/
+
+
+
+
+        leftDrive.setPower(-gamepad1.left_stick_y);
+        rightDrive.setPower(-gamepad1.right_stick_y);
+        pitch.setPower(-gamepad2.right_stick_y*0.4);
+        extension.setPower(-gamepad2.left_stick_y);
+        leftClaw.setPosition(gamepad2.right_trigger);
+        rightClaw.setPosition(gamepad2.right_trigger);
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("pitchMotor",pitch.getCurrentPosition() +"  " + pitch.getTargetPosition());
+//        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
